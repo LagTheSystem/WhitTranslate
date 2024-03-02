77f04eca0c5fd4c72e6751ba6832ddf9f980fbbb
LagTheSystem <lagthesystem@duck.com>
2024-03-01 20:25:49 -0600

    OBAMNA (plus small rewrite)

--- bot.py
+++ bot.py
@@ -2,28 +2,56 @@ import json
 import logging
 import discord as dc
 from discord import app_commands
+from discord.ext import tasks, commands
 
-handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 with open('config.json', 'r') as cfg:
     data = json.load(cfg)
 
 guild = dc.Object(id=1205245019703869460)
 
-intents = dc.Intents.default()
-intents.message_content = True
+bot = commands.Bot(command_prefix='!', intents=dc.Intents.all())
+tree = bot.tree
 
-client = dc.Client(command_prefix=pf, intents=intents)
-tree = app_commands.CommandTree(client)
-
-@tree.command(name="translate", description="Translate a message from or into Xander Talk", guild=guild)
+@bot.tree.command(name="translate", description="Translate a message from or into Xander Talk", guild=guild)
 @app_commands.describe(message='message')
 async def translate(interaction, message: str):
     print(message)
     await interaction.response.send_message(content=message[::-1], ephemeral=True)
 
-@client.event
-async def on_ready():
-    await tree.sync(guild=guild)
-    print(f'Bot Setup as {client.user}')
+@bot.tree.command(name="election", description="THIS COMMAND DOES NOT WORK AT ALL YET, DO NOT USE", guild=guild)
+@app_commands.describe()
+@app_commands.checks.has_permissions(manage_messages=True)
+async def election(interaction):
+    embed = dc.Embed(
+        title = "Election",
+        color = dc.Color.yellow()
+    )
+    embed.add_field(name="tester", value="test value")
+    await interaction.response.send_message(embed=embed)
 
-client.run(data["token"], log_handler=handler, log_level=logging.DEBUG)
+class BackgroundTasks(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @tasks.loop(minutes=3)
+    async def obamna(self):
+        print("Obamna Cog Task Activated")
+        channel = self.bot.get_channel(1212191353035825184)
+        await channel.send("obamna")
+
+    async def cog_load(self):
+        print("Cog Loaded!")
+        self.obamna.start()
+
+async def setup_hook() -> None:
+    await bot.tree.sync(guild=guild)
+
+@bot.event
+async def on_ready():
+    print(f'Bot Setup as {bot.user}')
+    await setup(bot)
+
+async def setup(bot: commands.Bot):
+    await bot.add_cog(BackgroundTasks(bot))
+
+bot.run(data["token"])
